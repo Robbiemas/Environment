8a2bd47f6bb2a8f8cd6f841e4660d8c9486a9cae
Robbiemas <robertmastio@gmail.com>
2020-07-05 02:23:03 -0500

    working ballchange

--- Main.py
+++ Main.py
@@ -65,19 +65,46 @@ class Quark(object):  #Define class/object
        # pygame.draw.circle(win, (255, 230, 0), (self.x, self.y), 3)
         pygame.draw.circle(win, self.color, (round(self.x), round(self.y)), self.radius, self.width)
 
-    def up(self, radius, width, color, charge):
+
+
+
+
+
+
+
+
+class Breed(object):
+    def __init__(self, radius, width, color, charge):
+     #   radius = 7
+      #  width = 1
+      #  color = (255, 140, 0)
+      #  charge = 0
+        self.radius = radius
+        self.width = width
+        self.color = color
+        self.charge = charge
+
+    def callBreed(self):
+        if breedType == "up":
+            self.up()
+        if breedType == "down":
+            self.down()
+        if breedType == "electron":
+            self.electron()
+
+    def up(self):
         self.radius = 5
         self.width = 0
         self.color = (255, 230, 0)
         self.charge = 20
 
-    def down(self, radius, width, color, charge):
-        self.radius = 3
+    def down(self):
+        self.radius = 4
         self.width = 0
         self.color = (20, 150, 220)
-        self.charge = 20
+        self.charge = -10
 
-    def electron(self, radius, width, color, charge):
+    def electron(self):
         self.radius = 2
         self.width = 0
         self.color = (255, 140, 0)
@@ -86,20 +113,23 @@ class Quark(object):  #Define class/object
 
 def redrawGameWindow():
     win.fill([0, 0, 0])
-    for quark in Quarks:
+    for quark in quarkList:
         quark.angleToTrajectory(quark.angle)
         quark.draw(win)
     pygame.display.update()
 
 
+
 # mainloop
 run = True
-Quarks = []
+quarkList = []
 randAngle = 0
 flag = False
 flag2 = True
 rotDeg = 0
 startTime = 0
+breedType = "up"
+picker = Breed(7, 1, (255, 140, 0), 0)
 
 
 
@@ -107,6 +137,7 @@ while run:
     clock.tick(60)
     keys = pygame.key.get_pressed()
     print(rotDeg)
+    picker.callBreed()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -120,23 +151,51 @@ while run:
         rotDeg = 0
     rotDeg += 3
 
-    for quark in Quarks:
+    for quark in quarkList:
+        quark.prevX = quark.x  #sets prev x for changeinx
+        quark.prevY = quark.y  #sets prev y for changeiny
         if ((screenWidth - quark.radius) > quark.x > quark.radius) and ((screenHeight - quark.radius) > quark.y > quark.radius):
           #  quark.angleToTrajectory(quark.angle)
-            quark.prevX = quark.x
-            quark.prevY = quark.y
+
            # quark.changexy()
-            quark.Trajectory(quark.trajX, quark.trajY)
+            quark.Trajectory(quark.trajX, quark.trajY)  #writes new xy location
         else:
-            Quarks.pop(Quarks.index(quark))
+
+            if quark.y >= (screenHeight - quark.radius):
+                quark.angle %= 180
+                quark.angle = 180 - quark.angle
+                quark.angleToTrajectory(quark.angle)
+                quark.Trajectory(quark.trajX, quark.trajY)
+            elif quark.y <= (quark.radius):
+                quark.angle %= 360
+                quark.angle = 360 - quark.angle
+                quark.angleToTrajectory(quark.angle)
+                quark.Trajectory(quark.trajX, quark.trajY)
+            elif quark.x >= (screenWidth - quark.radius):
+                quark.angle %= 360
+                quark.angle = 180 + (360 - quark.angle)
+                quark.angleToTrajectory(quark.angle)
+                quark.Trajectory(quark.trajX, quark.trajY)
+            elif quark.x <= (quark.radius):
+                quark.angle %= 360
+                quark.angle = 180 - quark.angle
+                quark.angleToTrajectory(quark.angle)
+                quark.Trajectory(quark.trajX, quark.trajY)
+            else:
+                quarkList.pop(quarkList.index(quark))
     # Bounce at angle with vel
+    if keys[pygame.K_1]:
+        breedType = "up"
+    if keys[pygame.K_2]:
+        breedType = "down"
+    if keys[pygame.K_3]:
+        breedType = "electron"
 
     if keys[pygame.K_SPACE]:
         if flag2:
             startTime = pygame.time.get_ticks()
-            breed = Quark.up
-        #    Quark.angleToTrajectory(Quarks[quark], rotDeg)
-            Quarks.append(Quark(round(screenWidth // 2), round(screenHeight // 2), rotDeg, 5, 10, 0, (255, 230, 0), 20))  ##spawn up but not referenced yet
+        #    Quark.angleToTrajectory(quarkList[quark], rotDeg)
+            quarkList.append(Quark(round(screenWidth // 2), round(screenHeight // 2), rotDeg, 5, picker.radius, picker.width, picker.color, picker.charge))  ##spawn up but not referenced yet
             flag2 = False
 
         if not flag2 and ((pygame.time.get_ticks() - startTime) >= 50):
